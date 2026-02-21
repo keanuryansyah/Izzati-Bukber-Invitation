aa1249a4cecd4510b4e76eee6f61350a8c7a9f71
keanuryansyah <keanukeanu1207@gmail.com>
2026-02-21 16:03:19 +0700

    Last Commit v8

--- api/index.php
+++ api/index.php
@@ -1,15 +1,17 @@
 <?php
 
-// 1. Arahkan folder penyimpanan ke /tmp agar tidak Error 500
-// (Vercel hanya mengizinkan penulisan di folder /tmp)
-putenv('VIEW_COMPILED_PATH=/tmp');
-putenv('SESSION_DRIVER=cookie');
-putenv('LOG_CHANNEL=stderr');
+// 1. Paksa Laravel menggunakan folder /tmp untuk menulis cache dan views
+$tmpDir = '/tmp';
+putenv("VIEW_COMPILED_PATH=$tmpDir");
+putenv("APP_CONFIG_CACHE=$tmpDir/config.php");
+putenv("APP_ROUTES_CACHE=$tmpDir/routes.php");
+putenv("APP_SERVICES_CACHE=$tmpDir/services.php");
+putenv("APP_PACKAGES_CACHE=$tmpDir/packages.php");
 
 // 2. Jalankan Autoload
 require __DIR__ . '/../vendor/autoload.php';
 
-// 3. Inisialisasi Aplikasi Laravel
+// 3. Inisialisasi Aplikasi
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 // 4. Jalankan Kernel
