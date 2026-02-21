72aa491569a4e0cf1d883615843a5beea9f91d28
keanuryansyah <keanukeanu1207@gmail.com>
2026-02-21 15:52:18 +0700

    Last Commit v5

--- api/index.php
+++ api/index.php
@@ -1,2 +1,18 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// Mengarahkan Vercel agar menganggap folder root sebagai pangkalan
+$root = __DIR__ . '/../';
+
+// Memanggil autoload langsung dari root
+require $root . 'vendor/autoload.php';
+
+// Menjalankan aplikasi Laravel
+$app = require_once $root . 'bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
